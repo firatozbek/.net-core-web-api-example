59fb216e25703b76832a82b9307d57b30f87f21c
firat <firat@Pitcher4s-Mac-mini.local>
2020-01-23 14:36:08 +0300

    google client edded

---
+++ Controllers/GoogleAuthController.cs
@@ -0,0 +1,16 @@
+using myapi.Models;
+using myapi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace myapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class GoogleAuthController : ControllerBase
+    {
+        
+    }
+
+}
